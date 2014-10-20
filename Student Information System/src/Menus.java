f79ab185215a1d53abe07c47f53c47f3be55950f
Erin.Maronick <emaronick@mullenmail.com>
2014-10-20 13:08:33 -0600

    created menus

---
+++ Student Information System/src/Menus.java
@@ -0,0 +1,13 @@
+
+public class Menus
+	{
+	
+	public static void printMainMenu()
+		{
+		System.out.println("What would you like to do?");
+		System.out.println();
+		System.out.println("	1) add or delete a student");
+		System.out.println("	2) change student grades or schedules");
+		System.out.println("	3) sort students");
+		}
+	}

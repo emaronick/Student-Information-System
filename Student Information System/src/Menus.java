6c5f63a8ed656ad96250b814dfa14e85ff5eac7e
Erin.Maronick <emaronick@mullenmail.com>
2014-10-20 13:18:29 -0600

    creating submenus

--- Student Information System/src/Menus.java
+++ Student Information System/src/Menus.java
@@ -1,6 +1,8 @@
-
+import java.util.Scanner;
 public class Menus
 	{
+	public static int answer1;
+	public static boolean acceptable = false;
 	
 	public static void printMainMenu()
 		{
@@ -9,5 +11,40 @@ public class Menus
 		System.out.println("	1) add or delete a student");
 		System.out.println("	2) change student grades or schedules");
 		System.out.println("	3) sort students");
+		}
+	
+	public static void determineSubMenu()
+		{
+		acceptable = false;
+		while(acceptable = false)
+			{
+			Scanner keyboard = new Scanner(System.in);
+			answer1 = keyboard.nextInt();
+			switch(answer1)
+				{
+				case 1:
+					{
+					acceptable = true;
+					}
+					break;
+				case 2:
+					{
+					acceptable = true;
+					}
+					break;
+				case 3:
+					{
+					acceptable = true;
+					}
+					break;
+				default:
+					{
+					System.out.println("I'm sorry, " + answer1 + " is not a valid entry. Please enter an integer between one and three.");
+					acceptable = false;
+					}
+					break;
+				}
+			}
+		System.out.println("good");
 		}
 	}
--- Student Information System/src/StudentInformationSystemRunner.java
+++ Student Information System/src/StudentInformationSystemRunner.java
@@ -5,6 +5,7 @@ public class StudentInformationSystemRunner
 	public static void main(String[] args)
 		{
 		Menus.printMainMenu();
+		Menus.determineSubMenu();
 		}
 
 	}

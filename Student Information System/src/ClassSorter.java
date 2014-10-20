69067d114e57cc7f70e171ba66c5d0da985feded
quinn.weigers <quinn.weigers@MU-C3-03.mullen.pvt.k12.co.us>
2014-10-20 13:28:01 -0600

    1st commit

---
+++ Student Information System/src/ClassSorter.java
@@ -0,0 +1,15 @@
+import java.util.Comparator;
+
+
+public class ClassSorter
+		{
+		public int compare (Student s1, Student s2)
+			{
+			if(s1.getClass() > s2.getClass())
+				return 1;
+			else
+				return-1;
+			}
+		}
+	
+	

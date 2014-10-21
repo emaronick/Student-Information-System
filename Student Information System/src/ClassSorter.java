8d2bead835fe1f9fab7392d9852e2d6a7122c607
quinn.weigers <quinn.weigers@MU-C3-03.mullen.pvt.k12.co.us>
2014-10-21 12:42:56 -0600

    completed the sorters

--- Student Information System/src/ClassSorter.java
+++ Student Information System/src/ClassSorter.java
@@ -6,5 +6,9 @@ public class ClassSorter
 		{
 		public int compare (StudentInfo s1, StudentInfo s2)
 			{
-			if(Menus.classSortChoice == 1) 
-				return s1.getClassP1().compareTo(s2.getClass)
+			//could be that classSortChoice isnt what menus has.
+			if(Menus.classSortChoice == 1) return s1.getClassP1().compareTo(s2.getClassP1());
+			else if(Menus.classSortChoice == 2) return s1.getClassP2().compareTo(s2.getClassP2());
+			else return s1.getClassP3().compareTo(s2.getClassP3());
+			}
+		}
